4ef6e5d93a3710096a03a2a353a1a1bb27c24d31
emanoelantonio <dev.emanoel@protonmail.com>
2022-01-20 10:15:26 -0300

    Implementing server development

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express, {Request, Response, NextFunction} from "express";
 const app = express();
 
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).send({ work: 'confirmed' });
+  res.status(200).send({ work: 'Servidor ativado com sucesso!!' });
 
 });
 
